21b309dd4113ab15d3b7921aed475bc9c01bd2d0
nafeesanaz <naznafisa53@gmail.com>
2021-04-11 22:39:09 +0100

    Registering to App

--- app/src/main/java/com/example/myfirebasechatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/myfirebasechatapp/RegisterActivity.kt
@@ -1,11 +1,52 @@
 package com.example.myfirebasechatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+
+    }
+
+    private fun registerUser(userName:String, email:String, password:String){
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){
+                    if (it.isSuccessful){
+                    val user: FirebaseUser? = auth.currentUser
+                        val userId: String = user!!.uid
+                        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
+
+                        val hashMap: HashMap < String, String> = HashMap()
+                        hashMap.put("userId", userId)
+                        hashMap.put("userName",userName)
+                        hashMap.put("profileImage","")
+
+                        databaseReference.setValue(hashMap).addOnCompleteListener(this){
+                            if (it.isSuccessful){
+                                //open home activity
+                                val intent = Intent(this@RegisterActivity, HomeActivity::class.java)
+                                startActivity(intent)
+                            }
+                        }
+                    }
+                }
     }
 }
